399e392e7a60f432da0a9428bff23ca2b4b920d7
Vu Duc Thuan <vuducthuan1994@gmail.com>
2020-10-03 22:21:34 +0700

    fix  ok

---
+++ public/js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+        navigation: true,
+        navigationPosition: 'left',
+        slidesNavigation: true,
+        slidesNavPosition: 'bottom',
+        controlArrows: true,
+        css3: true,
+        scrollingSpeed: 700,
+        autoScrolling: true,
+        lazyLoading: true
+    });
+});
