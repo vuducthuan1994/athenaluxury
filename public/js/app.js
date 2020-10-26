45429e9b3ddc40a8f1f81cfa2ca2a9d6099fbeef
Vũ Đức Thuận <vdthuan@vnpt.vn>
2020-10-26 14:11:49 +0700

    fix done

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    $(".preloader-container").delay(2500).fadeOut(500);
+    $(".preloader-container").delay(3000).fadeOut(500);
     initMenuMobile();
     owlApartmentInit();
     owlDevelopmentProjectInit();
